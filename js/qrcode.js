79e474f8c370965202f61da199f0ad38898752b0
Your Name <your.email@example.com>
2026-08-03 19:23:51 +0900

    qr_maker: 長い文字列でQRが生成されない問題を修正（容量超で暗号エラー→Too long data化、エラーレベルLへフォールバック、エラー表示追加）

--- js/qrcode.js
+++ js/qrcode.js
@@ -460,7 +460,7 @@ var QRCode;
 		var nType = 1;
 		var length = _getUTF8Length(sText);
 		
-		for (var i = 0, len = QRCodeLimitLength.length; i <= len; i++) {
+		for (var i = 0, len = QRCodeLimitLength.length; i < len; i++) {
 			var nLimit = 0;
 			
 			switch (nCorrectLevel) {
